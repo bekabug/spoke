130754f40fd87dafce53ffc4bc6c0dc82a8ceaa2
Beka <beka@bekabug.com>
2018-10-10 19:30:49 -0400

    giving up on jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-$('.calendar-element-numbers').html(function (i, html) {
-    return html.replace(/(\d)/g, '<span>$1</span>');
+jQuery(document).ready(function() {
+    $('.calendar-element-numbers').html(function (i, html) {
+        return html.replace(/(\d)/g, '<span>$1</span>');
+    });
 });
